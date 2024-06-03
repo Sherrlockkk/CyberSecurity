74bc84d2bd06ba653c97423699dd8c640dc0c231
Eren <140344483+Sherrlockkk@users.noreply.github.com>
2024-06-03 22:51:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///users.db')
+
+# MySQL bağlantı bilgilerini ayarlayın
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/credentials'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
